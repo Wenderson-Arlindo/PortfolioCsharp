ae6fb1ff11f69ddcaa388707a361f34fe3b3d8ba
W <wenderson250408@gmail.com>
2026-03-11 20:50:12 -0300

    Criar Models/Dados.cs

---
+++ AgiotaSystem/Models/Dados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Agiota.Models;
+
+class Dados
+{
+    public int Numero { get; set; }
+    public string Nome { get; set; }
+    public decimal Valor { get; set; }
+    public decimal Juros { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime DataVencimento { get; set; }
+
+    public Dados(int numero, string nome, decimal valor, decimal juros, DateTime dataa, DateTime dataf)
+    {
+        Numero = numero;
+        Nome = nome;
+        Valor = valor;
+        Juros = juros;
+        DataInicio = dataa;
+        DataVencimento = dataf;
+    }
+}
