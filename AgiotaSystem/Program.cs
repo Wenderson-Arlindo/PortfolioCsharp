8beb30be61896949d22eba0a9a4591cfab6ed40a
W <wenderson250408@gmail.com>
2026-03-11 20:43:15 -0300

    Criar AgiotaSystem/Program.cs

---
+++ AgiotaSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Agiota.UI;
+
+namespace Agiota;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Menu.Mostrar();
+        }
+    }
+}
